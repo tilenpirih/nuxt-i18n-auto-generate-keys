d27d71097b4f9e3d50ff0fdc82714cec74fc7f62
Tilen Pirih <tilen.pirih@gmail.com>
2026-06-14 14:58:35 +0200

    refactor: remove unused script and i18n keys from index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,21 +1,6 @@
-<script setup lang="ts">
-const { t } = useI18n({ useScope: 'local' })
-</script>
-
 <template>
     <div>
         <SwitchLanguage />
-        {{ t('Hello world') }}
+        {{ translationKeyInComposable() }}
     </div>
 </template>
-
-<i18n lang="json">
-{
-    "en": {
-        "Hello world": "Hello world"
-    },
-    "sl": {
-        "Hello world": "TODO_TRANSLATION: Hello world"
-    }
-}
-</i18n>
